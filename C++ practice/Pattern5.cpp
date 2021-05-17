0987acad5a41326b0d06d51ff2bb2e58b8c94ee4
Peter Johnson <53271626+peterjohnson1000@users.noreply.github.com>
2021-05-17 18:19:34 +0530

    Create Pattern5.cpp

---
+++ C++ practice/Pattern5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //  Inputting just number of rows(n) from the user
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) // this is to count the number of rows
+    {
+        for (int j = 1; j <= n; j++) //this is to count the no of starts in a row
+        {
+            if (j <= n - i)
+            {
+                cout << " ";
+            }
+            else
+            {
+                cout << "*";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+/*          *
+           **
+          ***
+         ****
+        *****     */
