deabf52e4ac1461fb5322921780f057f3ea3d411
Peter Johnson <53271626+peterjohnson1000@users.noreply.github.com>
2021-05-19 00:02:35 +0530

    Create Number Pattern.cpp

---
+++ C++ practice/Number Pattern.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //  Inputting just number of rows(n) from the user
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) // this is to count the number of rows
+    {
+        for (int j = 1; j <= 1; j++) //this is to count the no of starts in a row
+        {
+            for (int k = 1; k <= n - i; k++)
+            {
+                cout << " ";
+            }
+            for (int l = 1; l <= i; l++)
+            {
+                cout << l << " ";
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
+
+/*              1 
+               1 2 
+              1 2 3 
+             1 2 3 4 
+            1 2 3 4 5          */
